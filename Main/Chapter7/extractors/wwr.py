14d24c868abbd059db2925a176c9edeb73968eb9
Kimviruscool <sinsa9122@naver.com>
2025-11-07 22:10:08 +0900

    Chapter 7 flask & scraping indeed.py edit

---
+++ Main/Chapter7/extractors/wwr.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import cloudscraper
+
+def extract_wwr_jobs(keyword):
+    scraper = cloudscraper.create_scraper()
+    base_url = "https://weworkremotely.com/remote-jobs/search?term="
+    response = scraper.get(f"{base_url}{keyword}")
+    print("response.status_code=",response.status_code)
+
+    if response.status_code != 200:
+        print("Can't request website")
+    else:
+        results = []
+        soup = BeautifulSoup(response.text, "html.parser")
+        jobs = soup.find_all('section', class_="jobs")
+        for job_section in jobs:
+            job_posts = job_section.find_all('li')
+            job_posts.pop(-1)
+            for post in job_posts:
+                anchors = post.find_all('a')
+                anchor = anchors[1]
+                link = anchor['href']
+                company = post.find("p", class_="new-listing__company-name").text
+                region = post.find("p", class_="new-listing__company-headquarters").text
+                title = post.find('h4', class_='new-listing__header__title').text
+                job_data = {
+                    'link': f"https://weworkremotely.com{link}",
+                    'company': company.replace(",", " "),
+                    'location': region.replace(",", " "),
+                    'position': title.replace(",", " ")
+                }
+                results.append(job_data)
+    return results
---
+++ Main/Chapter7/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_job
+from extractors.wwr import extract_wwr_jobs
+
+app = Flask("JobScrapper")
+
+@app.route('/') #decorator #Syntactic sugar #데코레이터를 함수위에두면 user가 page를 방문했을때 호출해야하는 것을 알게됨
+def home():
+    return render_template("home.html", name="Bee")
+
+@app.route("/search")
+def hello():
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_job(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    jobs = indeed + wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+app.run("0.0.0.0")
